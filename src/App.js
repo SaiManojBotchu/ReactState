7e7d0e9c7390cff6b5f9fae9abba06eb7e7ee1d0
Sai Manoj <saimanoj2826@gmail.com>
2022-09-15 21:54:01 +0530

    setting state correctly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Game from './Game';
+// import Game from './Game';
 // import Game1 from './Game1';
+import Rando from './Rando';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Game />
-      {/* <Game1 /> */}
+      <Rando maxNum={10}/>
     </div>
   );
 }
